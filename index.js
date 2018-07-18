efe0de1c4db4492950bc1701eb7548d89f305e1b
Jeremy Goldstein <jeremysports7@gmail.com>
2018-07-18 05:41:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function sayHiToGrandma(string){
       return"I can't hear you!"
       }
   else if(string.toUpperCase() ===string){
-      console.log("YES INDEED!")
+      return"YES INDEED!"
       }
   else if(string === "I love you, Grandma."){
-       console.log("I love you, too.")
+       return"I love you, too."
   }
 }
